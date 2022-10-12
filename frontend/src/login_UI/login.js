67b6f26a38df5db4a4c11d38ff324fbc0a22e715
PhungNguyenHoang <89393736+PhungNguyenHoang@users.noreply.github.com>
2022-10-12 20:23:34 +0700

    hoang

--- frontend/src/login_UI/login.js
+++ frontend/src/login_UI/login.js
@@ -1,54 +1,71 @@
 import "./login.css";
 import React from "react";
-import axios from "axios";
-import * as mdb from "mdb-ui-kit"; // lib
-import { Input } from "mdb-ui-kit"; // module
-import { useNavigate } from "react-router";
+import axios from "../apis/api";
+
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function Login() {
   const [username, setUsername] = React.useState("");
   const [password, setpassword] = React.useState("");
-  const [error, setError] = React.useState("");
-  const [sucess, setsucess] = React.useState("");
-  const navigate = useNavigate();
 
-  const onChangeUsername = (e) => {
-    const value = e.target.value;
-    setUsername(value);
-  };
+  const formik =  useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .required("Không được trống")
+        .min(2, "Phải dài hơn 2 kí tự"),
+      password: Yup.string()
+        .required("Không được trống")
+        .matches(
+          /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+          "Mật khẩu không đúng định dạng"
+        ),
+    }),
 
-  const onChangePassword = (e) => {
-    const value = e.target.value;
-    setpassword(value);
-  };
-  const submitForm = async (e) => {
-    setError("");
-    setsucess("");
-    e.preventDefault();
-    // navigate("/");
-    window.location.href = "/medicine";
+   onSubmit: async  (values) => {
+    const { username, password } = values;
     try {
+      // const res = await axios({
+      //     url: '/api/auth/login',
+      //     method: 'post',
+      //     data: { username, password }
+      // });
+      
+      console.log({ username, password })
       const res = await axios({
-        url: "",
-        method: "POST",
-        data: { username, password },
+        url: '/api/auth/login',
+        method: 'post',
+        data: {
+          username,
+          password
+        }
       });
+      console.log("a");
 
       if (res.data.success) {
-        const user = {
-          username: res.data.data.username,
-          _id: res.data.data._id,
-        };
-        setsucess("Login successful");
-        localStorage.setItem("user", JSON.stringify(user));
-        window.location.href = "/";
+          const user = {
+              username: res.data.data.username,
+              _id: res.data.data._id,
+              
+          }
+          console.log("successfull")
+          localStorage.setItem('user', JSON.stringify(user));
+          //window.location.href = '/homepage'
       } else {
-        setError(res.data.data);
+        console.log("Loi")
       }
-    } catch (err) {
-      setError("loi roi");
-    }
-  };
+
+  } catch (err) {
+    console.log(err)
+  }
+ // window.location.href = '/'
+    },
+  });
+
 
   return (
     <section className="vh-100">
@@ -62,35 +79,43 @@ function Login() {
             />
           </div>
           <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-            <form onSubmit={submitForm}>
+            <form onSubmit={formik.handleSubmit}>
               <div className="divider d-flex align-items-center my-4">
-                <p className="text-center fw-bold mx-3 mb-0">Login</p>
+                <p className="text-center fw-bold mx-3 mb-0">Đăng Nhập</p>
               </div>
               {/* Email input */}
               <div className="form-outline mb-4">
                 <input
-                  type="email"
-                  id="form3Example3"
                   className="form-control form-control-lg"
-                  onChange={onChangeUsername}
                   placeholder="Enter a valid email address"
-                  required
+                  id="username"
+                  name="username"
+                  value={formik.values.username}
+                  onChange={formik.handleChange}
                 />
+                {formik.errors.name && (
+                  <p className="errorMsg"> {formik.errors.name} </p>
+                )}
                 <label className="form-label" htmlFor="form3Example3">
-                  Email address
+                  Email
                 </label>
               </div>
               {/* Password input */}
               <div className="form-outline mb-3">
                 <input
                   type="password"
-                  onChange={onChangePassword}
-                  id="form3Example4"
                   className="form-control form-control-lg"
                   placeholder="Enter password"
+                  id="password"
+                  name="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
                 />
+                {formik.errors.password && (
+                  <p className="errorMsg"> {formik.errors.password} </p>
+                )}
                 <label className="form-label" htmlFor="form3Example4" required>
-                  Password
+                  Mật Khẩu
                 </label>
               </div>
               <div className="d-flex justify-content-between align-items-center">
@@ -103,21 +128,21 @@ function Login() {
                     id="form2Example3"
                   />
                   <label className="form-check-label" htmlFor="form2Example3">
-                    Remember me
+                    Nhớ mật khẩu
                   </label>
                 </div>
-                <a href="#!" className="text-body">
-                  Forgot password?
+                <a href="/forgotpassword" className="text-body">
+                  Quên mật khẩu
                 </a>
               </div>
               <div className="text-center text-lg-start mt-4 pt-2">
                 <button
-                  type="button"
+                  type="onSubmit"
                   className="btn btn-primary btn-lg"
-                  style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
-                  onClick ={submitForm}
+                  href="/homepage"
+                  styleFor={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
                 >
-                  Login
+                  Đăng Nhập
                 </button>
               </div>
             </form>
