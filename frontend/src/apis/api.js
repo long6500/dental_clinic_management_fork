b9334f56192d30c9399f3bbc3dccd1fa6c40defa
PhungNguyenHoang <89393736+PhungNguyenHoang@users.noreply.github.com>
2022-10-19 16:36:22 +0700

    add

--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -2,6 +2,25 @@ import axios from "axios";
 
 // const baseURL = process.env.BASE_URL;
 
-export default axios.create({
+const ins = axios.create({
     baseURL: `http://localhost:9000/`
   });
+
+  ins.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('token');
+    if (!token) return config;
+  
+    config.headers['authorization'] = `Bearer ${token}`;
+    return config;
+  
+  }, function (error) {
+    return Promise.error(error);
+  })
+  
+  ins.interceptors.response.use(function (response) {
+    return response.data;
+  }, function (error) {
+    return Promise.reject(error);
+  })
+  
+  export default ins;
